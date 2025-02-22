4590348a64877453d4518863fc1649d95372fa64
Bjørn Göttler <bjoern@goettler.dk>
2025-02-22 20:00:34 +0100

    Mock: Create mocks for repositories

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -4,6 +4,8 @@ using HotelBooking.UnitTests.Fakes;
 using Xunit;
 using System.Linq;
 using System.Threading.Tasks;
+using Moq;
+using System.Collections.Generic;
 
 
 namespace HotelBooking.UnitTests
@@ -11,16 +13,88 @@ namespace HotelBooking.UnitTests
     public class BookingManagerTests
     {
         private IBookingManager bookingManager;
-        IRepository<Booking> bookingRepository;
+        private Mock<IRepository<Room>> mockRoomRepository;
+        private Mock<IRepository<Booking>> mockBookingRepository;
 
         public BookingManagerTests(){
             DateTime start = DateTime.Today.AddDays(10);
             DateTime end = DateTime.Today.AddDays(20);
-            bookingRepository = new FakeBookingRepository(start, end);
-            IRepository<Room> roomRepository = new FakeRoomRepository();
-            bookingManager = new BookingManager(bookingRepository, roomRepository);
-        }
+            mockRoomRepository = new Mock<IRepository<Room>>();
+            mockBookingRepository = new Mock<IRepository<Booking>>();
+            
+            var rooms = new List<Room>
+            {
+                new Room { Id = 1, Description = "Room 1" },
+                new Room { Id = 2, Description = "Room 2" }
+            };
+            var customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Id = 1,
+                    Name = "Customer One",
+                    Email = "customer_one@example.org"
+                },
+                new Customer
+                {
+                    Id = 2,
+                    Name = "Customer Two",
+                    Email = "customer_two@example.org"
+                }
+            };
 
+            var bookings = new List<Booking>
+            {
+                new Booking
+                {
+                    Id = 1,
+                    StartDate = start,
+                    EndDate = end,
+                    IsActive = true,
+                    CustomerId = customers[0].Id,
+                    RoomId = 1
+                },
+                new Booking
+                {
+                    Id = 2,
+                    StartDate = start,
+                    EndDate = end,
+                    IsActive = true,
+                    CustomerId = customers[1].Id,
+                    RoomId = 2
+                }
+            };
+            
+            mockRoomRepository.Setup(x => x.GetAllAsync()).Returns(Task.FromResult<IEnumerable<Room>>(rooms));
+            mockRoomRepository.Setup(x => x.GetAsync(It.IsInRange(1, 2, Moq.Range.Inclusive))).Returns(Task.FromResult<Room>(rooms[0]));
+            mockRoomRepository.Setup(x => x.RemoveAsync(It.IsInRange(1, 2, Moq.Range.Inclusive))).Returns(Task.CompletedTask);
+
+            mockBookingRepository.Setup(x => x.GetAsync(It.IsInRange(1, 2, Moq.Range.Inclusive))).Returns(Task.FromResult<Booking>(bookings[0]));
+            mockBookingRepository.Setup(x => x.GetAllAsync()).Returns(Task.FromResult<IEnumerable<Booking>>(bookings));
+            mockBookingRepository.Setup(x => x.AddAsync(It.IsAny<Booking>())).Returns(Task.CompletedTask);
+            mockBookingRepository.Setup(x => x.RemoveAsync(It.IsInRange(1, 2, Moq.Range.Inclusive))).Returns(Task.CompletedTask);
+            mockBookingRepository.Setup(x => x.EditAsync(It.IsAny<Booking>())).Returns(Task.CompletedTask);
+            
+            //bookingRepository = new FakeBookingRepository(start, end);
+            //IRepository<Room> roomRepository = new FakeRoomRepository();
+            bookingManager = new BookingManager(mockBookingRepository.Object, mockRoomRepository.Object);
+        }
+        
+        
+        [Theory]
+        [InlineData("13/12/2024", "12/12/2024")]
+        public async Task FindAvailableRoom_StartDateLaterThanEndDate_ThrowsArgumentException(string startDateString,
+            string endDateString)
+        {
+            // Arrange
+            var startDate = DateTime.Parse(startDateString);
+            var endDate = DateTime.Parse(endDateString);
+            // Act
+            Task result() => bookingManager.FindAvailableRoom(startDate, endDate);
+            // Assert
+            await Assert.ThrowsAsync<ArgumentException>(result);
+        }
+        
         [Fact]
         public async Task FindAvailableRoom_StartDateNotInTheFuture_ThrowsArgumentException()
         {
@@ -44,7 +118,7 @@ namespace HotelBooking.UnitTests
             // Assert
             Assert.NotEqual(-1, roomId);
         }
-
+        
         [Fact]
         public async Task FindAvailableRoom_RoomAvailable_ReturnsAvailableRoom()
         {
@@ -57,7 +131,7 @@ namespace HotelBooking.UnitTests
             // Act
             int roomId = await bookingManager.FindAvailableRoom(date, date);
 
-            var bookingForReturnedRoomId = (await bookingRepository.GetAllAsync()).
+            var bookingForReturnedRoomId = (await mockBookingRepository.Object.GetAllAsync()).
                 Where(b => b.RoomId == roomId
                            && b.StartDate <= date
                            && b.EndDate >= date
